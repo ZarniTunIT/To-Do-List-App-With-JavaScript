4d6f1373e7c28afa5fa262cb82082aa7df0ece5e
ZarniTunIT <mgmgzarniygn123@gmail.com>
2022-02-20 08:51:39 +0630

    added delete_all feature

--- js/app.js
+++ js/app.js
@@ -9,12 +9,23 @@ function task() {
     del.onclick = function () {
         del.parentNode.remove();
     };
+    document.querySelector("#delete-all").style.display = "block";
     document.querySelector("ul").appendChild(task);
     task.appendChild(del);
     document.querySelector("#new").value = "";
     document.querySelector("#new").focus();
 };
+
+function deleteAll() {
+    let tasks = document.querySelector("#tasks");
+    while (tasks.firstChild) {
+        tasks.removeChild(tasks.firstChild);
+    }
+    document.querySelector("#delete-all").style.display = "none";
+};
+
 document.querySelector("button").onclick = task;
 document.querySelector("#new").onkeydown = function (e) {
     if (e.which === 13) task();
+    document.querySelector("#delete-all").onclick = deleteAll;
 };
